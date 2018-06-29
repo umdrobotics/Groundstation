00ac168e2b8cbcdd94863b64a65d9b13d155b461
Yi Feng <fenyi@umich.edu>
2018-06-29 16:29:23 -0400

    add exception handlers for socket

--- apriltags2_ros/apriltags2_ros/scripts/Apriltag_tcpclient.py
+++ apriltags2_ros/apriltags2_ros/scripts/Apriltag_tcpclient.py
@@ -14,46 +14,50 @@ BUFFER_SIZE = 1024
 
 # Setup ROS
 rospy.init_node('Apriltag_server', anonymous=True)
-pub1 = rospy.Publisher("/usb_cam/tag_detections_socket", PoseStamped, 10)
-
+pub1 = rospy.Publisher("/usb_cam/tag_detections_from_socket", PoseStamped, 10)
 print "Initialized ROS, try to connect......"
+
 
 # Setup Socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((TCP_IP, TCP_PORT))
-print "Connected to client:", addr, "! Starting sending data."
+try:
+    s.connect((TCP_IP, TCP_PORT))
+except socket.error:
+    raise ValueError("TIMEOUT, check the ip addr and connection! ")
+print "Connected to server! Start receiving data."
 
-vecTagDetections = AprilTagDetectionArray()
 
-
+# Start processing data and publish msg
 while 1:
+
     data = s.recv(BUFFER_SIZE)
 
-    if not data: break
+    # Deal with exceptions
+    if not data:    continue
+    if not ("###" in data and "!!!" in data):     continue
 
-    splitData = PoseStamped()
+
+    vecTagDetections = PoseStamped()
 
     # Try to split data
-    if "###" in data and "!!!" in data:
-        
-        indicesMsgHead = [m.start() for m in re.finditer("###", data)]
-        indicesMsgEnd = [m.start() for m in re.finditer("!!!", data)]
+    indicesMsgHead = [m.start() for m in re.finditer("###", data)]
+    indicesMsgEnd = [m.start() for m in re.finditer("!!!", data)]
 
-        firstMsg = data[indicesMsgHead[0]+3 : indicesMsgEnd[0]]
-        indicesComma = [m.start() for m in re.finditer(",", data)]
+    firstMsg = data[indicesMsgHead[0]+3 : indicesMsgEnd[0]]
+    indicesComma = [m.start() for m in re.finditer(",", data)]
 
-        if len(indicesComma) != 6:
-            raiseError("Msg inside is wrong")
+    if len(indicesComma) != 6:
+        continue
 
-        vecTagDetections.pose.position.x    = double(data[0 : indicesComma[0]])
-        vecTagDetections.pose.position.y    = double(data[indicesComma[0] : indicesComma[1]])
-        vecTagDetections.pose.position.z    = double(data[indicesComma[1] : indicesComma[2]])
-        vecTagDetections.pose.orientation.x = double(data[indicesComma[2] : indicesComma[3]])
-        vecTagDetections.pose.orientation.y = double(data[indicesComma[3] : indicesComma[4]])
-        vecTagDetections.pose.orientation.z = double(data[indicesComma[4] : indicesComma[5]])
-        vecTagDetections.pose.orientation.w = double(data[indicesComma[5] : -1])
-        pub1.publish(vecTagDetections)
-        rospy.loginfo(vecTagDetections)
+    vecTagDetections.pose.position.x    = double(data[0 : indicesComma[0]])
+    vecTagDetections.pose.position.y    = double(data[indicesComma[0] : indicesComma[1]])
+    vecTagDetections.pose.position.z    = double(data[indicesComma[1] : indicesComma[2]])
+    vecTagDetections.pose.orientation.x = double(data[indicesComma[2] : indicesComma[3]])
+    vecTagDetections.pose.orientation.y = double(data[indicesComma[3] : indicesComma[4]])
+    vecTagDetections.pose.orientation.z = double(data[indicesComma[4] : indicesComma[5]])
+    vecTagDetections.pose.orientation.w = double(data[indicesComma[5] : -1])
+    pub1.publish(vecTagDetections)
+    rospy.loginfo(vecTagDetections)
 
 
 
--- apriltags2_ros/apriltags2_ros/scripts/Apriltag_tcpserver.py
+++ apriltags2_ros/apriltags2_ros/scripts/Apriltag_tcpserver.py
@@ -13,9 +13,10 @@ BUFFER_SIZE = 1024
 # Core function: everytime received a msg, send it out in socket
 def callback(vecTagDetections):
 
-    if not vecTagDetections:
+    if not vecTagDetections: # if vector is empty
         return
 
+    # Encode data and publish
     data = "###"
     data = data + str(vecTagDetections.detections[0].pose.pose.pose.position.x) + ","
     data = data + str(vecTagDetections.detections[0].pose.pose.pose.position.y) + ","
@@ -32,17 +33,25 @@ def callback(vecTagDetections):
 
 # Setup ROS
 rospy.init_node('Apriltag_server', anonymous=True)
-
 print "Initialized ROS, waiting for connections......"
 
 
 # Setup Socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((TCP_IP, TCP_PORT))
-s.listen(1)
-        
-conn, addr = s.accept()
-print "Connected to client:", addr, "! Starting sending data."
+err_log = s.bind((TCP_IP, TCP_PORT))
+if err_log != 0:
+    print "Error in binding."
+    conn.close()
+    
+err_log = s.listen(5) # non-blocking, just tell the kernel how many clients to wait
+if err_log != 0:
+    print "Error in listening."
+    conn.close()
+       
+conn, addr = s.accept() # blocking, until this func get one conn & addr from the establised connections queue
+print "Connected to client:", addr, "! Start sending data."
+
+
 
 sub1 = rospy.Subscriber("/usb_cam/tag_detections", AprilTagDetectionArray, callback)
 
